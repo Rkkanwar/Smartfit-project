5c9395204b89173fba4c08a8ea07d4afce767f1e
Prem-sharma34 <ps4183085@gmail.com>
2024-12-06 16:20:36 +0530

    added the full functionlity of the signup and login page and logout button

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for,flash,redirect,flash , session
 from data import exercises
+import sqlite3
+
 
 app = Flask(__name__)
+app.secret_key = 'asdf'
+
+# Initialize database
+def init_db():
+    conn = sqlite3.connect('database.db')
+    conn.execute('''  CREATE TABLE IF NOT EXISTS users(
+                        id integer primary key autoincrement,
+                        name text not null,
+                        email text unique not null,
+                        password text not null
+                    )''')
+    conn.close()
+
+init_db()
+
 
 @app.route("/")
 def home():
@@ -14,16 +31,58 @@ def main():
     return render_template("main.html")
 
 
-@app.route("/signup")
+@app.route("/signup" , methods=['GET','POST'])
 def signup():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        print(f"{name} and {email} and {password}")
+        try:
+            conn= sqlite3.connect('database.db')
+            cusor=conn.cursor()
+            cusor.execute("insert into users (name,email,password) values (?,?,?)",(name,email,password))
+            conn.commit()
+            conn.close()
+            flash("Signup successful! Please login.","success")
+            return redirect(url_for('login'))
+        except sqlite3.IntegrityError:
+            flash('email already exists!' ,"error")
+            
     return render_template("signup.html")
 
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        conn= sqlite3.connect('database.db')
+        cursor=conn.cursor()
+        cursor.execute("select password from users where email = ?",(email,))
+        record = cursor.fetchone()
+        conn.close()
+        
+        
+        if record and password:
+            session['email'] = email
+            flash("Login successful!" , "success")
+            print("success")
+            return redirect(url_for("main"))
+        else:
+            flash("Invalid email or password!" , "error")
+            print("error")
+            
     return render_template("login.html")
 
+            
+@app.route('/logout')
+def logout():
+    session.pop('email',None)
+    flash("You have been logged out." , "info")
+    return redirect(url_for('home'))
 
 @app.route("/chatbot")
 def chatbot():
